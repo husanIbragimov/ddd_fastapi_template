856a8bd6f50655fbec7920fc91de50f0bab19c9d
Husan Ibragimov <husandev@mail.ru>
2025-09-28 13:51:03 +0500

    feat: integrate structured logging with `structlog` for improved log management
    - Added `structlog` to dependencies in `requirements.txt`.
    - Implemented `configure_logging` in `utils/logger.py` for flexible log formatting (console or JSON based on `DEBUG` setting).

---
+++ src/utils/logger.py
@@ -0,0 +1,18 @@
+import structlog
+from core.settings import settings
+
+logger = structlog.get_logger()
+
+def configure_logging():
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.dev.ConsoleRenderer() if settings.DEBUG else structlog.processors.JSONRenderer()
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
